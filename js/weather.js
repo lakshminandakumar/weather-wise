6d00cd25d2bda14a00f521945d2b99ba34aca158
Lakshmi Nandakumar <lakshmi.nandakumar@drvnsolutions.com>
2023-06-27 21:20:04 +0100

    added some more styling

--- js/weather.js
+++ js/weather.js
@@ -31,16 +31,20 @@ export async function checkWeather(city){
             appCard.style = "background: linear-gradient(200deg, #ee9f4f, #258701)";
         }else if(data.weather[0].main == "Clear"){
             weatherIcon.src = "images/clear.png";
-            appCard.style = "background: linear-gradient(135deg, #00feba, #5b548a)";
+            // appCard.style = "background: linear-gradient(135deg, #00feba, #5b548a)";
+            appCard.style = "background: linear-gradient(175deg, #1ce1ef, #6bc0f1, #258701)";
         }else if(data.weather[0].main == "Rain"){
             weatherIcon.src = "images/rain.png";
-            appCard.style = "background: linear-gradient(175deg, #424645, #6f6e3b, #2d2560)";
+            appCard.style = "background: linear-gradient(175deg, #272727, #6f6e3b, #2d2560)";
         }else if(data.weather[0].main == "Drizzle"){
             weatherIcon.src = "images/drizzle.png";
+            appCard.style = "background: linear-gradient(175deg, #6d6666, #6f6e3b, #035325)";
         }else if(data.weather[0].main == "Mist"){
             weatherIcon.src = "images/mist.png";
+            appCard.style = "background: linear-gradient(175deg, afb2b1, #6f6e3b)";
         }else if(data.weather[0].main == "Snow"){
             weatherIcon.src = "images/snow.png";
+            appCard.style = "background: linear-gradient(175deg, #afd7ee, #c8c8c5)";
         }
 
         document.querySelector(".weather").style.display = "block";
